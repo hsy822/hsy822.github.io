7730c48d0af81bb560b4bf63046f1ce03ec52bb7
unknown <hyunsy822@gmail.com>
2019-03-26 19:52:51 +0900

    first commit

---
+++ precache-manifest.996db18e5c78dab73fc3ef5bd5d23ab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e7b96164d0fdcf976eeb3af16fb252f",
+    "url": "/static/media/image1.5e7b9616.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f364c81e43cf1469b4fd",
+    "url": "/static/js/main.f364c81e.chunk.js"
+  },
+  {
+    "revision": "2cc9dd1c416b187089d0",
+    "url": "/static/js/1.2cc9dd1c.chunk.js"
+  },
+  {
+    "revision": "f364c81e43cf1469b4fd",
+    "url": "/static/css/main.f73a9ee0.chunk.css"
+  },
+  {
+    "revision": "0b9ecc94818e1548bdf09a150cf5b9df",
+    "url": "/index.html"
+  }
+];
